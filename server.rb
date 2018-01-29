634e5b4b6326a25a24877b47e9eb5c8f564e7492
Denis <denisglb@yahoo.co.uk>
2018-01-29 19:19:12 +0000

    pointing class

--- server.rb
+++ server.rb
@@ -12,6 +12,7 @@ class WitchHunt
 end
 
 class DirectionProcess
+	attr_reader :curr_position
 
 	def initialize
 	@array = ["forward", "right", "forward", "forward", "forward", "left", "forward", "forward", "left", "right", "forward", "right", "forward", "forward", "right", "forward", "forward", "left"]
@@ -21,36 +22,32 @@ class DirectionProcess
 
 	def pointing
 		
-		p forward = @array.each_with_index.map { |a, i| a == 'forward' ? 1 : a }
+		@array.each do |i| 
+			if i == "left"
+				@curr_position -= 90
+				update
 
+			elsif i == "right" 
+				@curr_position += 90
+				update
 
+			else
+				i == "forward"
+				@move << [i, curr_position]
+			end
+		end
+		p @move
 	end
 end
+
+def update
+	if curr_position == -90 then @curr_position = 270 end
+	if curr_position == 360 then @curr_position = 0 end
+end
+
 
 # witch_hunt = WitchHunt.new
 # p witch_hunt.directions
 
 direction_process = DirectionProcess.new
 direction_process.pointing
-
-
-# class StackExchange
-#   include HTTParty
-#   base_uri 'api.stackexchange.com'
-
-#   def initialize(service, page)
-#     @options = { query: { site: service, page: page } }
-#   end
-
-#   def questions
-#     self.class.get("/2.2/questions", @options)
-#   end
-
-#   def users
-#     self.class.get("/2.2/users", @options)
-#   end
-# end
-
-# stack_exchange = StackExchange.new("stackoverflow", 1)
-# puts stack_exchange.questions
-# puts stack_exchange.users
