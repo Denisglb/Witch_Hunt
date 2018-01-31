130cb19e2fb58123e2b633b4869febaf83db8f50
Denis <denisglb@yahoo.co.uk>
2018-01-31 16:54:21 +0000

    testing the controller initial method

--- spec/co_ordinantes_spec.rb
+++ spec/co_ordinantes_spec.rb
@@ -8,5 +8,13 @@ describe CoOrdinantes do
 		it "is initialized with an empty array" do
 			expect(co_ordinates.direction).to be_an_instance_of(Array)
 		end
+
+		it "has an initial value of x at 0" do 
+			expect(co_ordinates.x_value).to be_equal(0)
+		end
+
+		it "has an initial value of y at 0" do
+			expect(co_ordinates.y_value).to be_equal(0)
+		end
 	end
 end
